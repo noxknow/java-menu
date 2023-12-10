4e29f428664a5972db3e4f68c16835922a9735cc
noxknow <chris2769@naver.com>
2023-12-10 16:33:13 +0900

    test(CoachWithRestrictedMenuTest) : 메뉴 입력 형식이 다른 경우 예외가 발생한다

---
+++ src/test/java/menu/domain/wrapper/CoachWithRestrictedMenuTest.java
@@ -0,0 +1,32 @@
+package menu.domain.wrapper;
+
+import menu.handler.ErrorHandler;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+public class CoachWithRestrictedMenuTest {
+
+    private static Stream<Arguments> testMenuFormat() {
+        return Stream.of(
+                Arguments.of("토미", "우동-,스시"),
+                Arguments.of("제임스", "뇨끼1,월남쌈"),
+                Arguments.of("포비", "AAA,고추잡채")
+        );
+    }
+
+    @DisplayName("메뉴 입력 형식이 다른 경우 예외가 발생한다.")
+    @ParameterizedTest(name = "[{index}] input {0}")
+    @MethodSource("testMenuFormat")
+    void createMenuWithInvalidMenuFormat(String coachName, String restrictedMenu) {
+        assertThatThrownBy(() -> CoachWithRestrictedMenu.of(coachName, restrictedMenu))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorHandler.INVALID_MENU_FORMAT.getException().getMessage());
+    }
+
+}
