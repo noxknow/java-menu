5fd6bb1d8f933ebb4c51dd915944e44d8692e9d3
noxknow <chris2769@naver.com>
2023-12-10 18:16:37 +0900

    test(CoachWithRestrictedMenuTest) : 못 먹는 메뉴가 중복된다면 예외가 발생한다

--- src/test/java/menu/domain/wrapper/CoachWithRestrictedMenuTest.java
+++ src/test/java/menu/domain/wrapper/CoachWithRestrictedMenuTest.java
@@ -35,8 +35,15 @@ public class CoachWithRestrictedMenuTest {
         );
     }
 
+    private static Stream<Arguments> testMenuDuplicate() {
+        return Stream.of(
+                Arguments.of("토미", "우동,우동"),
+                Arguments.of("제임스", "월남쌈,월남쌈")
+        );
+    }
+
     @DisplayName("메뉴 입력 형식이 다른 경우 예외가 발생한다.")
-    @ParameterizedTest(name = "[{index}] input {0}")
+    @ParameterizedTest(name = "[{index}] input {1}")
     @MethodSource("testMenuFormat")
     void createMenuWithInvalidMenuFormat(String coachName, String restrictedMenu) {
         assertThatThrownBy(() -> CoachWithRestrictedMenu.of(coachName, restrictedMenu))
@@ -45,7 +52,7 @@ public class CoachWithRestrictedMenuTest {
     }
 
     @DisplayName("메뉴가 존재하지 않는 경우 예외가 발생한다.")
-    @ParameterizedTest(name = "[{index}] input {0}")
+    @ParameterizedTest(name = "[{index}] input {1}")
     @MethodSource("testExistMenu")
     void createMenuWithNonExistentMenu(String coachName, String restrictedMenu) {
         assertThatThrownBy(() -> CoachWithRestrictedMenu.of(coachName, restrictedMenu))
@@ -54,11 +61,20 @@ public class CoachWithRestrictedMenuTest {
     }
 
     @DisplayName("메뉴의 개수가 0과 2사이가 아니라면 예외가 발생한다.")
-    @ParameterizedTest(name = "[{index}] input {0}")
+    @ParameterizedTest(name = "[{index}] input {1}")
     @MethodSource("testMenuRange")
     void createMenuWithInvalidMenuRange(String coachName, String restrictedMenu) {
         assertThatThrownBy(() -> CoachWithRestrictedMenu.of(coachName, restrictedMenu))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ErrorHandler.INVALID_MENU_RANGE.getException().getMessage());
+    }
+
+    @DisplayName("못 먹는 메뉴가 중복된다면 예외가 발생한다.")
+    @ParameterizedTest(name = "[{index}] input {1}")
+    @MethodSource("testMenuDuplicate")
+    void createMenuWithDuplicate(String coachName, String restrictedMenu) {
+        assertThatThrownBy(() -> CoachWithRestrictedMenu.of(coachName, restrictedMenu))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorHandler.DUPLICATE_MENU.getException().getMessage());
     }
 }
